c005ffe7b8f3692a5114fab3e59f60f3b746f80b
Sungha Choi <lime.choish2003@gmail.com>
2026-01-29 12:58:02 -0800

    initial project

---
+++ Untitled.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+library(readr)
+
+df <- read_csv("data/raw/penguins.csv")
+graph <- ggplot(df, aes(x=flipper_length_mm, y = body_mess_g)) + geom_point()
+ggsave("output/plot.png")
+
